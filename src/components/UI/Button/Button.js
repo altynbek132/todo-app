7ec640fb6684141d9cf666cedfa2b93978685e2a
Altynbek <altynbek.ktl@gmail.com>
2020-06-01 21:57:25 +0600

    button ui

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -5,13 +5,17 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 const Button = ({ onClick, children, type, isDisabled }) => {
-  const cls = ['button', type];
+  const cls = ['Button', type];
   return (
     <button className={cls.join()} onClick={onClick} disabled={isDisabled}>
       {children}
     </button>
   );
 };
+
+Button.propTypes = {};
+
+Button.defaultProps = {};
 
 const mapStateToProps = (state) => ({});
 
