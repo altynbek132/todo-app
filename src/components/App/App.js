43a9e7b1513583453630cec5224db8fdd34fa86c
Altynbek <altynbek.ktl@gmail.com>
2020-06-05 11:51:31 +0600

    classnames, cx bind in template

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,14 @@
-import cls from './App.scss';
+import styles from './App.scss';
 
+import classNames from 'classnames/bind';
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
+const cx = classNames.bind(styles);
+
 const App = ({}) => {
-  return <div className={cls['App']}>App</div>;
+  return <div className={cx('App')}>App</div>;
 };
 
 const mapStateToProps = ({}) => ({});
